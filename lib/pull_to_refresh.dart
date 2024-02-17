293592e6cdd208cca019877a8790367e0f5eb4b7
hiren surani <hirensurani82@gmail.com>
2024-02-17 23:57:05 +0530

    17_2_20224

---
+++ lib/pull_to_refresh.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PullToRefresh extends StatefulWidget {
+  const PullToRefresh({super.key});
+
+  @override
+  State<PullToRefresh> createState() => _PullToRefreshState();
+}
+
+class _PullToRefreshState extends State<PullToRefresh> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
