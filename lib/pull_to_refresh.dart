2743268d14bbb767c2470e10cad5e034621f51ef
hiren surani <hirensurani82@gmail.com>
2024-02-18 01:18:24 +0530

    17_2_20224

--- lib/pull_to_refresh.dart
+++ lib/pull_to_refresh.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class PullToRefresh extends StatefulWidget {
@@ -8,14 +10,42 @@ class PullToRefresh extends StatefulWidget {
 }
 
 class _PullToRefreshState extends State<PullToRefresh> {
+  List<String> items = ['Item 1','Item 2','Item 3'];
+
+  Future refresh() async {
+    setState(() {
+      items.clear();
+    });
+    final url =  Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    final response = await http.get(url);
+
+    if(response.statusCode == 200){
+      final List newItems = json.decode(response.body);
+      setState(() {
+        items = newItems.map((e) {
+          final number = e['id'];
+          return 'Item $number';
+        }).toList();
+      });
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
+      appBar: AppBar(title: const Text(" Pull To Refresh"),),
+      body:items.isEmpty?const Center(child: CircularProgressIndicator(),):RefreshIndicator(
+        onRefresh: refresh,
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+          final item = items[index];
 
-        ],
-      ),
+          return ListTile(title: Text(item),);
+        },),
+      )
     );
   }
 }
